edc512b7fd42d13520a89eff7152a987d3133c98
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-05-12 21:57:10 -0700

    Add MainPageTestSuite class

---
+++ src/test/java/com/wiley/MainPageTestSuite.java
@@ -0,0 +1,83 @@
+package com.wiley;
+
+import org.junit.Test;
+
+
+public class MainPageTestSuite {
+    @Test
+    public void should_WhoWEServe_link_is_displayed_in_The_top_Menu() {
+    }
+
+    @Test
+    public void should_Subjects_link_is_displayed_in_the_top_menu() {
+    }
+
+    @Test
+    public void should_TheWileyNetwork_link_is_displayed_in_the_top_menu() {
+    }
+
+    @Test
+    public void should_About_link_is_displayed_in_the_top_menu() {
+    }
+
+    @Test
+    public void should_Contact_link_is_displayed_in_the_top_menu() {
+    }
+
+    @Test
+    public void should_Help_link_is_displayed_in_the_top_menu() {
+    }
+
+    @Test
+    public void should_MyAccount_link_is_displayed_in_the_top_menu() {
+    }
+
+    @Test
+    public void should_be_item_Students_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Instructors_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_BookAuthors_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Professionals_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Researchers_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Institutions_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Librarians_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Corporations_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Societies_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_JournalEditors_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Bookstores_under_WhoWEServe_for_subHeader() {
+    }
+
+    @Test
+    public void should_be_item_Government_under_WhoWEServe_for_subHeader() {
+    }
+
+}
