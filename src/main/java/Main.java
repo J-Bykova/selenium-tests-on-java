baf762ecf5aed32979c0282c09318146585255aa
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-05-12 20:30:37 -0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.gecko.driver", "/Users/jenny/Code/IntelliJIDEA/com.github/driver/geckodriver");
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        MainPage mainPage = new MainPage(driver);
+        driver.get("https://github.com/");
+        mainPage.clickSingIn();
+        driver.close();
+
+
+    }
+}
