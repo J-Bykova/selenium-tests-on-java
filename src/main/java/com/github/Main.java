8cb49f99d84f5b2a24e77c63b189700211e252e2
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-05-12 20:43:40 -0700

    Refactoring

--- src/main/java/com/github/Main.java
+++ src/main/java/com/github/Main.java
@@ -1,3 +1,5 @@
+package com.github;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
--- src/main/java/com/github/MainPage.java
+++ src/main/java/com/github/MainPage.java
@@ -1,3 +1,5 @@
+package com.github;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
