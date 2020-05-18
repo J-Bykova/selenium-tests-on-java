ad239b16b0fddbbd5233783d0f6025e1465e2bbe
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-05-17 18:42:12 -0700

    Add SonyXperiaPage

---
+++ src/main/java/com/demoguru99/pages/SonyXperiaPage.java
@@ -0,0 +1,11 @@
+package com.demoguru99.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SonyXperiaPage {
+    WebDriver driver;
+
+    public SonyXperiaPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
