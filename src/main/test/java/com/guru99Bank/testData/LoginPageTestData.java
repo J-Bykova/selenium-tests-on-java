c7fb12fa23fbc9701deebd36ab139d7690c9bc22
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-05-18 20:55:09 -0700

    Add value for errorTextForInvalidLoginIn

--- src/main/test/java/com/guru99Bank/testData/LoginPageTestData.java
+++ src/main/test/java/com/guru99Bank/testData/LoginPageTestData.java
@@ -3,7 +3,8 @@ package com.guru99Bank.testData;
 public class LoginPageTestData {
     public static String validUserId = "mngr261185";
     public static String validPassword = "Unamazu";
-    public static String inValidUserId = "Jenny";
-    public static String inValidPassword = "12345";
+    public static String invalidUserId = "Jenny";
+    public static String invalidPassword = "12345";
     public static String expectedTitleHomePage = "Manger Id : mngr261185";
+    public static String errorTextForInvalidLoginIn = "User or Password is not valid";
 }
