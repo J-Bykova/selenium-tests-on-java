3c8bf1f65c8a69133b673556b482946fd6185f06
Jenny Bykova <eva.gre4ka@yandex.ru>
2020-05-23 18:24:24 -0700

    Add tearDown class

--- src/main/test/java/com/guru99Bank/HomePageTestSuite.java
+++ src/main/test/java/com/guru99Bank/HomePageTestSuite.java
@@ -1,7 +1,10 @@
 package com.guru99Bank;
 
+import com.github.pages.LoginPage;
 import com.guru99Bank.pages.HomePage;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -11,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class HomePageTestSuite {
     private WebDriver driver;
     private HomePage homePage;
+    private LoginPage loginPage;
 
     @Before
     public void setUp() {
@@ -18,7 +22,13 @@ public class HomePageTestSuite {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("http://www.demo.guru99.com/V4/");
+        driver.get("http://www.demo.guru99.com/V4/manager/Managerhomepage.php");
         homePage = PageFactory.initElements(driver, HomePage.class);
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 }
